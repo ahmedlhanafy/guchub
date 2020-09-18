61be7e6a5d1b444b04737bc964acf5bf577ae9b4
Ahmed Elhanafy <ahelhana@microsoft.com>
2020-09-18 23:15:38 +0200

    Test pipeline

--- config/web/App.js
+++ config/web/App.js
@@ -8,10 +8,10 @@ if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker
       .register('/service-worker.js')
-      .then(registration => {
+      .then((registration) => {
         console.log('SW registered: ', registration);
       })
-      .catch(registrationError => {
+      .catch((registrationError) => {
         console.log('SW registration failed: ', registrationError);
       });
   });
